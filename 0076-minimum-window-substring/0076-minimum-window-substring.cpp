1b99d937094c50986cd28c5aade28d9a5d4635d9
Abhijith.SR <120040613+Abhijithsr13@users.noreply.github.com>
2024-12-10 00:40:22 +0530

    Time: 0 ms (100%), Space: 11.7 MB (8.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         unordered_map<char, int> requiredChars;
-    int formedChars = 0;
-    int left = 0, right = 0;
-    int minLen = INT_MAX;
-    int minWindowStart = 0;
+        int minLen = INT_MAX;
+        int sIndex = -1;
+        int hash[256] = {0};
 
-    // Initialize the required characters count
-    for (char ch : t) {
-        requiredChars[ch]++;
-    }
-
-    while (right < s.length()) {
-        // Expand the window
-        if (requiredChars.find(s[right]) != requiredChars.end()) {
-            requiredChars[s[right]]--;
-            if (requiredChars[s[right]] >= 0) {
-                formedChars++;
-            }
+        for (char c : t) {
+            hash[c]++;
         }
 
-        // Contract the window
-        while (formedChars == t.length()) {
-            // Update the minimum window substring
-            if (right - left + 1 < minLen) {
-                minLen = right - left + 1;
-                minWindowStart = left;
-            }
+        int count = 0;
+        int l = 0, r = 0;
 
-            // Shrink the window
-            if (requiredChars.find(s[left]) != requiredChars.end()) {
-                requiredChars[s[left]]++;
-                if (requiredChars[s[left]] > 0) {
-                    formedChars--;
+        while (r < s.size()) {
+            if (hash[s[r]] > 0) {
+                count++;
+            }
+            hash[s[r]]--;
+
+            while (count == t.size()) {
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    sIndex = l;
                 }
+                hash[s[l]]++;
+                if (hash[s[l]] > 0) {
+                    count--;
+                }
+                l++;
             }
-
-            left++;
+            r++;
         }
 
-        right++;
-    }
-    return minLen == INT_MAX ? "" : s.substr(minWindowStart, minLen);
+        return (sIndex == -1) ? "" : s.substr(sIndex, minLen);
     }
 };
