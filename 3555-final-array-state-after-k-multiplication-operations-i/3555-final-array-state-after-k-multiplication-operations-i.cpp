5fc75ec41bf2bf01555b788b8fed4527940e568c
Abhijith.SR <120040613+Abhijithsr13@users.noreply.github.com>
2024-12-16 22:03:08 +0530

    Time: 4 ms (20.56%), Space: 27.9 MB (20.77%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        for (int i = 0; i < k; ++i) {
-        int minIndex = 0;
-        for (int j = 1; j < nums.size(); ++j) {
-            if (nums[j] < nums[minIndex]) {
-                minIndex = j;
-            }
-        }
-        nums[minIndex] *= multiplier;
+         // Create a priority queue (min-heap) storing pairs of (value, index)
+    priority_queue<pair<int, int>,vector<pair<int, int>>, greater<>> minHeap;
+
+    // Initialize the min-heap with the values and their indices
+    for (int i = 0; i < nums.size(); ++i) {
+        minHeap.emplace(nums[i], i);
     }
+
+    // Perform k operations
+    for (int i = 0; i < k; ++i) {
+        // Get the minimum value and its index
+        auto [value, index] = minHeap.top();
+        minHeap.pop();
+
+        // Update the value in the nums array
+        nums[index] *= multiplier;
+
+        // Push the updated value back into the min-heap
+        minHeap.emplace(nums[index], index);
+    }
+
     return nums;
     }
 };
