250fb602ce28b60a973d49c355ba6fd28cd31261
Abhijith.SR <120040613+Abhijithsr13@users.noreply.github.com>
2024-12-18 22:27:31 +0530

    Time: 0 ms (100%), Space: 14.1 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int>ans;
+         vector<int>ans;
         int n=prices.size();
         for(int i=0;i<n;i++){
             int dis=prices[i];
