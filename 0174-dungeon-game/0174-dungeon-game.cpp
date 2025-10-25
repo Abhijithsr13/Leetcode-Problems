cf1adb9119125bf6523339c4dbb1d9bd4a933354
Abhijith.SR <120040613+Abhijithsr13@users.noreply.github.com>
2025-10-25 23:09:33 +0530

    Time: 0 ms (100%), Space: 13.1 MB (50.11%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int getVal(vector<vector<int>> &mat, vector<vector<int>> &dp , int i=0, int j=0)
+    {
+        int n = mat.size();
+        int m = mat[0].size();    
+        
+        if(i == n || j == m)    return 1e9; 
+        
+        if(i == n-1 and j == m-1)
+            return (mat[i][j] <= 0) ? -mat[i][j] + 1 : 1;
+        
+        if( dp[i][j] != 1e9)
+            return dp[i][j];
+        
+        int IfWeGoRight = getVal(mat , dp , i , j+1);
+        int IfWeGoDown = getVal(mat , dp , i+1 , j);
+        
+        int minHealthRequired =  min(IfWeGoRight , IfWeGoDown) -mat[i][j];
+        
+        dp[i][j] = ( minHealthRequired <= 0 ) ? 1 : minHealthRequired;      
+        return dp[i][j];
+    }
+    
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+        
+        vector<vector<int>> dp(n , vector<int>(m , 1e9));
+        
+        return getVal(dungeon, dp);     
+    }
+};
